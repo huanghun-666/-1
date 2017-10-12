496d98ccb28f879201d4123005991ba4fb3e8fc4
姚立飞 <huanghun-666@163.com>
2017-10-12 23:10:06 +0800

    Initial Commit

---
+++ 太阳系1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  太阳系1
+//
+//  Created by 姚立飞 on 2017/10/12.
+//  Copyright © 2017年 姚立飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 太阳系1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  太阳系1
+//
+//  Created by 姚立飞 on 2017/10/12.
+//  Copyright © 2017年 姚立飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
